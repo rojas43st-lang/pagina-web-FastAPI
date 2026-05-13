ef4420243c5085e6d428088ac550895924d30850
rojas43st-lang <rojas43st@gmail.com>
2026-05-13 19:13:12 +0000

    Fix deploy

--- api/index.py
+++ api/index.py
@@ -1,20 +1,9 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 
 app = FastAPI()
 
-# Archivos estáticos
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Templates HTML
-templates = Jinja2Templates(directory="templates")
-
-
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
-    )
+@app.get("/")
+async def home():
+    return {
+        "message": "FastAPI funcionando en Vercel 🚀"
+    }
