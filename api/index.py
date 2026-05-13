7de3af092e9aa227a118fd70d3cd496d4765ae88
rojas43st-lang <rojas43st@gmail.com>
2026-05-13 19:25:50 +0000

    Fix templates vercel

--- api/index.py
+++ api/index.py
@@ -1,20 +1,12 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-# Archivos estáticos
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Templates HTML
-templates = Jinja2Templates(directory="templates")
 
-
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
-    )
+@app.get("/")
+async def root():
+    return FileResponse("templates/index.html")
