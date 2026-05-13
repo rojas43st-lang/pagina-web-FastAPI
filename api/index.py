dc9c6719292a66a1a0ff011d61e7d98c10ead42d
rojas43st-lang <rojas43st@gmail.com>
2026-05-13 19:21:40 +0000

    Pagina funcionando en vercel

--- api/index.py
+++ api/index.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return {
-        "message": "FastAPI funcionando en Vercel 🚀"
-    }
+# Archivos estáticos
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Templates HTML
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
