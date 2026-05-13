cee8dd49c054e5554982de09143bac5662b9b1d9
rojas43st-lang <rojas43st@gmail.com>
2026-05-13 19:48:08 +0000

    Fix Jinja rendering

--- api/index.py
+++ api/index.py
@@ -1,11 +1,44 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# Archivos estáticos
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Templates
+templates = Jinja2Templates(directory="templates")
+
+
 @app.get("/")
-async def home():
-    return FileResponse("templates/index.html")
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
+
+@app.get("/modelos")
+async def modelos(request: Request):
+    return templates.TemplateResponse(
+        "modelos.html",
+        {"request": request}
+    )
+
+
+@app.get("/tecnologia")
+async def tecnologia(request: Request):
+    return templates.TemplateResponse(
+        "tecnologia.html",
+        {"request": request}
+    )
+
+
+@app.get("/historia")
+async def historia(request: Request):
+    return templates.TemplateResponse(
+        "historia.html",
+        {"request": request}
+    )
+    
